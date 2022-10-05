8438f2e808a67560c32614c1380827dee3fd5bb7
Eshaan Singh Parihar <eshaansinghparihar@esh.local>
2022-10-05 20:18:12 +0530

    fixed landing

--- src/Activities.js
+++ src/Activities.js
@@ -24,7 +24,7 @@ function Activities({activity, user , notifyDeleteSuccess})
     }
     return(
         <div className="form">
-            {(activity.imageUrl)?<img className="activity" src={activity.imageUrl} alt={activity.name}/>:<img src="https://source.unsplash.com/1600x1600/?food" alt={activity.name}/>}
+            {(activity.imageUrl)?<img className="activity" src={activity.imageUrl} alt={activity.name}/>:<img className="activity" src="https://source.unsplash.com/1600x1600/?food" alt={activity.name}/>}
             {activity.calories<0?<h1 className="loss">{activity.calories} kcal</h1>:<h1 className="gain">{activity.calories} kcal</h1>}
             <h2>{activity.name}</h2>
             <h2>{new Intl.DateTimeFormat('en-US', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' , hour: '2-digit', minute: '2-digit'}).format(activity.timestamp)}</h2>
